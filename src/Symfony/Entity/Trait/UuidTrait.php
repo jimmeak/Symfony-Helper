a5421e1385c51a495299b099699f127c174a9220
Bára Václav <vaclav.bara@vlmedia.cz>
2022-12-01 22:12:32 +0100

    Uuid Trait with Set and Refresh Methods

--- src/Symfony/Entity/Trait/UuidTrait.php
+++ src/Symfony/Entity/Trait/UuidTrait.php
@@ -4,6 +4,7 @@ namespace Jimmeak\Symfony\Entity\Trait;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Types\UuidType;
+use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Uid\UuidV4;
 
 trait UuidTrait
@@ -17,5 +18,17 @@ trait UuidTrait
     public function getId(): UuidV4
     {
         return $this->id;
+    }
+
+    public function setId(UuidV4 $uuid): static
+    {
+        $this->uuid = $uuid;
+        return $this;
+    }
+
+    public function refreshUuid(): static
+    {
+        $this->uuid = Uuid::v4();
+        return $this;
     }
 }
