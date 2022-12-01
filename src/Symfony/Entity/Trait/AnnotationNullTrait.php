a5233ee95016f0fce17bbbca25f42890c1e343f2
Bára Václav <vaclav.bara@vlmedia.cz>
2022-12-01 22:22:21 +0100

    Anotation and Text Nullable added

---
+++ src/Symfony/Entity/Trait/AnnotationNullTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jimmeak\Symfony\Entity\Trait;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait AnnotationNullTrait
+{
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private string $annotation;
+
+    public function getAnnotation(): string
+    {
+        return $this->annotation;
+    }
+
+    public function setAnnotation(string $annotation): self
+    {
+        $this->annotation = $annotation;
+        return $this;
+    }
+}
---
+++ src/Symfony/Entity/Trait/TextNullTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Jimmeak\Symfony\Entity\Trait;
+
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+trait TextNullTrait
+{
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private string $text;
+
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+        return $this;
+    }
+}
